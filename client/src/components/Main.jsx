dd4c9c120866ceb5c6aa2e8bdfda9ef9eab0a09e
amatranga <amatranga892@gmail.com>
2018-03-06 19:46:02 -0600

    Finalizes Main.jsx component

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import Link from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-const Main = () => {
+const Main = (props) => {
   return (
-    <div className="row">
+    <div className="row text-center">
       <div className="col-12">
         <h1>Welcome!</h1>
       </div>
       <div className="col-12">
         <Link to="/ledger/simple-ledger">Simple Ledger</Link>
+      </div>
+      <div className="col-12">
         <Link to="/ledger/duplicate-ledger">Duplicate Ledger</Link>
+      </div>
+      <div className="col-12">
         <Link to="/ledger/complicated-ledger">Complicated Ledger</Link>        
       </div>
     </div>
